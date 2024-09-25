9a33874d08b3575dda936c77f9d3fcd70b819fc5
plinygg <aidan.chang@outlook.com>
2024-09-24 20:51:46 -0500

    moved to working on bfs solver

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 from random import choice
+from collections import deque
 
 RES = WIDTH, HEIGHT = 725, 725
-TILE = 25
+TILE = 75
 COLS, ROWS = WIDTH // TILE, HEIGHT // TILE
 
 pygame.init()
@@ -17,13 +18,17 @@ class Cell:
         self.walls = {"top": True, "bottom": True, "right": True, "left": True}
         self.visited = False #has the cell been visited/unvisited
         self.end = False # if the cell is the end cell (when to end the search program)
+        self.bfsvisited = False
+        self.dfsvisited = False
 
     def draw(self):
         x, y = self.x * TILE, self.y * TILE
-        if self.end:
-            pygame.draw.rect(screen, 'red', (x, y, TILE, TILE))
         if self.visited:
             pygame.draw.rect(screen, 'white', (x, y, TILE, TILE))
+
+        if x == (COLS - 1) * TILE and y == (ROWS - 1) * TILE:
+            self.end == True
+            pygame.draw.rect(screen, 'red', (x, y, TILE, TILE))
 
         if self.walls['top']:
             pygame.draw.line(screen, 'black', (x, y), (x + TILE, y), 2)
@@ -37,6 +42,10 @@ class Cell:
     def draw_current_cell(self):
         x, y = self.x * TILE, self.y * TILE
         pygame.draw.rect(screen, 'green', (x+2, y+2, TILE - 2, TILE - 2))
+
+    def draw_current_path_cell(self):
+        x, y = self.x * TILE, self.y * TILE
+        pygame.draw.rect(screen, 'orange', (x+2, y+2, TILE - 2, TILE - 2))
     
     def check_cell(self, x, y):
         if (x < 0 or x > COLS - 1 or y < 0 or y > ROWS - 1):
@@ -80,6 +89,25 @@ def remove_walls(current, next):
         current.walls['bottom'] = False
         next.walls['top'] = False
 
+def bfs(root):
+    q = deque
+    q.append(root)
+    res = []
+
+    while q:
+        pop = q.popleft()
+        pop.draw_current_path_cell()
+        if pop.end:
+            return False
+        if not root.walls['top']:
+            q.append(self.check_cell(self.x, self.y - 1))
+        if not root.walls['bottom']:
+            q.append(self.check_cell(self.x, self.y + 1))
+        if not root.walls['left']:
+            q.append(self.check_cell(self.x - 1, self.y))
+        if not root.walls['right']:
+            q.append(self.check_cell(self.x + 1, self.y))
+    #    root.bfsvisited = True
 
 grid_cells = [Cell(col, row) for row in range(ROWS) for col in range(COLS)]
 current_cell = grid_cells[0]
@@ -93,6 +121,8 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_q]:
         running = False
+    if keys[pygame.K_b]:
+        bfs(grid_cells[0])
 
 
     screen.fill('gray')
@@ -101,11 +131,14 @@ while running:
     current_cell.draw_current_cell()
 
     next_cell = current_cell.check_neighbors(grid_cells)
-    if next_cell:
+    if next_cell: # there are no longer any next cells
         next_cell.visited = True
+        stack.append(current_cell)
         remove_walls(current_cell, next_cell)
         current_cell = next_cell
+    elif stack:
+        current_cell = stack.pop()
 
 
     pygame.display.flip()
-    dt = clock.tick(60)
+    dt = clock.tick(100)
